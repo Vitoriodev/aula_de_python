be7f628507aafce3545d28fe83f3c764d1278c56
Vitorio_Augusto <160076193+Vitoriodev@users.noreply.github.com>
2024-10-11 22:29:47 -0300

    .

---
+++ extensivo.py
@@ -0,0 +1,10 @@
+
+""" isinstance:
+
+#você pode verifica ser a variavel esta com type certo em uma condição.
+num = int(input("coloque um numero"))
+
+if isinstance(num, int) == True:
+    print("certo")
+#posso fazer assim: isinstance(num, (int, float))
+"""
